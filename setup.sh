0138e37f3075d95345f208ce86421e248c8c27a6
Stevolus <25365857+Stevolus@users.noreply.github.com>
2023-09-11 17:14:19 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,13 +1,26 @@
 #!/bin/bash
 
-#Update repositories and packages
+#Update and upgrade packages
 apt update && apt -y upgrade
 
-#Add colours to prompt
+
+# -=My dot files=-
+
+#Add colours
 echo "export PS1="\[\e[32m\][\[\e[m\]\[\e[31m\]\u\[\e[m\]\[\e[33m\]@\[\e[m\]\[\e[32m\]\h\[\e[m\]:\[\e[36m\]\w\[\e[m\]\[\e[32m\]]\[\e[m\]\[\e[32;47m\]\\$\[\e[m\] "" > ~/.bashrc
+
+#Make du sort by size, show date, set size in MB and only show for the current folder.
+echo "alias du="du -hd1 --time --block-size=MB | sort -hr"" > ~/.bashrc
+
+#ZSH like CD commands for going back more than one directory 
+echo "alias cd"
+
+#Apply above .bashrc settings
+source ~/.bashrc
 
 #Install Micro editor
 curl https://getmic.ro | bash
 
 #Install LAMP?
-apt install -y tasksel && apt tasksel install lamp-server
+#apt install -y tasksel && apt tasksel install lamp-server
+
